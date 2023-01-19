541b748ca43dcaae177b4a7c64711ff740aa1f94
Oleg Khristenk <olegkhristenko@gmail.com>
2023-01-19 22:19:02 +0200

    Fixed error with status 500

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -7,5 +7,5 @@ module.exports = function (err, req, res, next) {
         return res.status(err.status).json({ message: err.message, errors: err.errors })
     }
 
-    return res.status(500).json({ message: 'Неочікувана помилка' });
+    return res.status(500).json({ message: 'Неочікувана помилка...' });
 };
