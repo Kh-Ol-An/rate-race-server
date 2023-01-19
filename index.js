8cd69a23a1e28f6437998eb02d085499f1517450
Oleg Khristenk <olegkhristenko@gmail.com>
2023-01-19 21:45:06 +0200

    Fixed cors 4

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors({
     origin: process.env.CLIENT_URL,
 }));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
