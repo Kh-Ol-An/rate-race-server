25c987a533a4f0e0a53445e24319273bb67b715b
Oleg Khristenk <olegkhristenko@gmail.com>
2023-01-19 22:07:25 +0200

    show error

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -1,7 +1,7 @@
 const ApiError = require('../exceptions/api-error');
 
 module.exports = function (err, req, res, next) {
-    console.log(err);
+    console.log('errorMiddleware: ', err);
 
     if (err instanceof ApiError) {
         return res.status(err.status).json({ message: err.message, errors: err.errors })
