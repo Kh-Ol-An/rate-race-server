9d6254b874ebf410a4d10258fab78a2b57c1b0f5
Oleg Khristenk <olegkhristenko@gmail.com>
2023-01-19 21:40:41 +0200

    Fixed cors 3

--- index.js
+++ index.js
@@ -12,17 +12,23 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.options(process.env.CLIENT_URL, function(req, res, next){
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'POST');
-    res.header("Access-Control-Allow-Headers", "accept, content-type");
-    res.header("Access-Control-Max-Age", "1728000");
-    return res.sendStatus(200);
-});
+// app.options(process.env.CLIENT_URL, function(req, res, next){
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Methods', 'POST');
+//     res.header("Access-Control-Allow-Headers", "accept, content-type");
+//     res.header("Access-Control-Max-Age", "1728000");
+//     return res.sendStatus(200);
+// });
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
 }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/api', router);
 app.use(errorMiddleware);
 
