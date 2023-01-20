b1ae8bfa2375440868cae2331d485bcfefd315d8
Oleg Khristenk <olegkhristenko@gmail.com>
2023-01-20 09:14:05 +0200

    Fixed error with status 500 6

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -1,14 +1,12 @@
 const ApiError = require('../exceptions/api-error');
 
 module.exports = function (err, req, res, next) {
-    // console.log('errorMiddleware start');
     console.log(err);
 
     if (err instanceof ApiError) {
         // console.log('errorMiddleware if');
         return res.status(err.status).json({ message: err.message, errors: err.errors })
     }
-    // console.log('errorMiddleware end');
-    //
+
     // return res.status(500).json({ message: 'Неочікувана помилка...' }).render('error', { error: err });
 };
