2737cafe438b6bcc156e1905f7c93ea7a0c030ea
Oleg Khristenk <olegkhristenko@gmail.com>
2023-01-28 10:10:59 +0200

    Added name to UserDto

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -1,9 +1,11 @@
 module.exports = class UserDto {
+    name;
     email;
     id;
     isActivated;
 
     constructor(model) {
+        this.name = model.name;
         this.email = model.email;
         this.id = model._id;
         this.isActivated = model.isActivated;
